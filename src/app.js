f2568cc664724d8a3165e2a9d3b778bbed8d4136
Harsh Singh <harshsinghujjain07@gmail.com>
2023-10-26 23:31:14 +0530

    added screenshots

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const port = 8000;
 const hbs = require("hbs");
 
-//seeting view engine
+//seting view engine
 const templatePath = path.join(__dirname,'../templates/views')
 app.set('view engine','hbs');
 app.set('views', templatePath);
